eed59c7259e5550526dfeea9206ef0f6a576a691
tuyou <yoututouch@gmail.com>
2014-11-28 13:42:09 +0800

    First commit

---
+++ PhotoTweaks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoTweaks
+//
+//  Created by Tu You on 14/11/28.
+//  Copyright (c) 2014年 Tu You. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoTweaks/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PhotoTweaks
+//
+//  Created by Tu You on 14/11/28.
+//  Copyright (c) 2014年 Tu You. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
